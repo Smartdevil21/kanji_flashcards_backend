7120192c5387c1658cb0d2717df1e04d041c4f8e
Pratik1803 <pratikvaidya1803@gmail.com>
2022-07-31 18:21:52 +0530

    ahgsfhasd

--- src/handlers/user/verifyUserEmail.handler.ts
+++ src/handlers/user/verifyUserEmail.handler.ts
@@ -13,7 +13,8 @@ export async function verifyUserEmailHandler(
             { returnNewDocument: true }
         );
         if (!result) throw new Error("User not found!");
-        res.status(200).sendFile(path.join(__dirname, "/index.html"));
+        // res.status(200).sendFile(path.join(__dirname, "/index.html"));
+        res.status(200).send("Email verified Successfully!");
     } catch (error) {
         console.log(`Err in GET /user/verify-email : ${error}`);
         res.status(400).json({ success: false, data: { message: `${error}` } });
